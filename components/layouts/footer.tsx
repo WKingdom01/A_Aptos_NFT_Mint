3e0dab5fd259d772fea1764a4afb4f1fb73fe0f3
WKingdom01 <charliebrown.work1100@gmail.com>
2022-10-27 18:12:42 +0900

    dmytro

--- components/layouts/footer.tsx
+++ components/layouts/footer.tsx
@@ -5,7 +5,7 @@ const Footer = () => {
   return (
     <div className="fixed bottom-0">
       <p>
-        @Built by Charlie Brown, <a href="mailto:charliebrownbusiness0011@gmail.com">charliebrownbusiness0011@gmail.com</a> 
+        @ Built by Dmytro  Mokrenko
       </p>
     </div>
   );
